5037f68d094ab8e03f99055d29f430beade20342
KIguchi <kiguchi118@gmail.com>
2020-01-03 01:36:26 +0900

    [Finish] 5-3

--- Rails/work/vagrant/meshiterro/config/initializers/application_controller_renderer.rb
+++ Rails/work/vagrant/meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '49af8752726bc9a3593c3fb1f1ba1c7976e54f61ec8b00d45143e34e2e20b09e9267ed325967523d4d31166294e07c1a56be91426bcffeccab4eadbfeb2f5a13'
