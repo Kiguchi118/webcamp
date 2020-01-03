122da7efb85d79a726578d1a895f6cdc2c15a9d0
KIguchi <kiguchi118@gmail.com>
2020-01-04 07:52:55 +0900

    [Finish] Bookers2

--- Rails/work/vagrant/Bookers2/app/controllers/users_controller.rb
+++ Rails/work/vagrant/Bookers2/app/controllers/users_controller.rb
@@ -5,12 +5,12 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   	@books = @user.books
-  	@new_book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
+  	@new_book = Book.new
   end
 
   def index
-  	@users = User.all #一覧表示するためにUserモデルのデータを全て変数に入れて取り出す。
-  	@new_book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
+  	@users = User.all
+  	@new_book = Book.new
   end
 
   def edit
@@ -34,7 +34,6 @@ class UsersController < ApplicationController
   	params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
-  #url直接防止　メソッドを自己定義してbefore_actionで発動。
    def baria_user
   	unless params[:id].to_i == current_user.id
   		redirect_to user_path(current_user)
