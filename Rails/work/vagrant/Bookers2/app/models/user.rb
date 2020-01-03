c7892c491b624566530a6440cdf79243f0ec7006
KIguchi <kiguchi118@gmail.com>
2020-01-04 07:53:30 +0900

    [Finish] Bookers2

--- Rails/work/vagrant/Bookers2/app/models/user.rb
+++ Rails/work/vagrant/Bookers2/app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable,:validatable
-
-  has_many :books
-  attachment :profile_image, destroy: false
-
-  validates :name, presence: true, length: {maximum: 20, minimum: 2}
-  validates :introduction, length: {maximum: 50}
+         :recoverable, :rememberable, :validatable
 end
