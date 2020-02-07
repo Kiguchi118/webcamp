47599475525e8b3018112f396d800aa7eff3a31d
KIguchi <kiguchi118@gmail.com>
2020-02-07 11:34:17 +0900

    Finish[6-7]

--- JavaScript/5/app.js
+++ JavaScript/5/app.js
@@ -2,7 +2,7 @@ $(function () {
     $('.box1').slideDown(function () {
         $('.box1').css({
             'background-color': '#0000FF',
-            'width': '200px',
+            // 'width': '200px',
             //'height': '100px'
         }).slideUp();
     });
